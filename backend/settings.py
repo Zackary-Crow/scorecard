4497a5337cd80c21a57247cf9dd27fa93a96a241
Zackary Crow <zackcrow@rocketmail.com>
2023-11-06 19:42:45 -0500

    update debug

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(6nofwezj2sx2b7j8vw(7^*rz5$h@ln$qbn%t1mv*=u70f)_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
